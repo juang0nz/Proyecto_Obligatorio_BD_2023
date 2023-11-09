632ddb944abfc329dfa1ea32d28fdb17f032e927
SantiRDL <santiago.rodriguezdl@correo.ucu.edu.uy>
2023-11-09 17:42:08 -0300

    DB sql

---
+++ db_obligatorio/Obligatorio.sql
@@ -0,0 +1,39 @@
+#INICIO
+CREATE DATABASE obligatorio;
+USE obligatorio;
+
+CREATE TABLE Logins(
+    LogId VARCHAR(200) not null,
+    Password VARCHAR(200) not null
+);
+
+CREATE TABLE Funcionarios(
+    Ci INTEGER not null, #Primary key
+    nombre VARCHAR(50) not null,
+    apellido VARCHAR(50) not null,
+    fecha_nacimiento VARCHAR(50) not null,
+    direccion VARCHAR(50) not null,
+    Telefono INTEGER not null,
+    email VARCHAR(50) not null,
+    LogId VARCHAR(200) not null #Foreign key
+);
+
+CREATE TABLE Logins(
+    Nro INTEGER not null,
+    Ci INTEGER not null,
+    Fch_Agenda VARCHAR(200) not null
+);
+
+CREATE TABLE Carnet_Salud(
+    Ci INTEGER not null,
+    Fch_Emision VARCHAR(200) not null,
+    Fch_Vencimiento VARCHAR(200) not null,
+    Comprobante VARCHAR(200) not null #Link a imagen????
+);
+
+CREATE TABLE Periodos_Actualizacion(
+    Year VARCHAR(200) not null,
+    Semestre VARCHAR(200) not null,
+    Fch_Inicio VARCHAR(200) not null,
+    Fch_Fin VARCHAR(200) not null
+);
